32ce6ec6a0c5d8744ac77df13d644153a383556e
DiegoMontes20 <65183020+DiegoMontes20@users.noreply.github.com>
2021-11-22 14:46:26 -0600

    First commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+let contextSW = '/20213-PWA-U2-P7/sw.js';
+let url = window.location.href;
+
+let player = $('#player');
+let photoUser = $('#photoUser');
+
+let btnCamera = $('#btnCamera');
+let btnCameraBack = $('#btnCameraBack');
+let btnTakePhoto = $('#btnTakePhoto');
+
+const camera = new Camera(player[0]);
+
+btnCamera.on('click', () => {
+  camera.on().then((result) => {
+    if (!result) {
+      alert('Error al iniciar la cámara');
+    }
+  });
+});
+
+btnCameraBack.on('click', () => {
+  console.log('camara back');
+  camera.onBack().then((result) => {
+    if (!result) {
+      alert('Error al iniciar la cámara');
+    }
+  });
+});
+
+btnTakePhoto.on('click', () => {
+  camera.off();
+  photoUser.attr('src', camera.takePhoto());
+});
+
+if (navigator.serviceWorker) {
+  if (url.includes('localhost')) {
+    contextSW = '/sw.js';
+  }
+  navigator.serviceWorker.register(contextSW);
+}
